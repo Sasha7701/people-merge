23048d3b45c95f1d3a1fc9991b09d8293295e89b
Alex Polotsky <polonyc77@gmail.com>
2017-05-10 10:51:35 -0400

    people merge

---
+++ people_merge.js
@@ -0,0 +1,24 @@
+const fs = require ('fs');
+
+//	read me files from json - contains people's names
+
+const people1 = require ("./people1.json");
+const people2 = require ("./people2.json");
+ console.log(people1);
+ console.log(people2);
+//
+let peopleMerge = people1.concat(people2).sort();
+//console.log(peopleMerge.sort());
+let output = '';
+
+for (var i = 0; i < peopleMerge.length; i++) {
+	 output += peopleMerge[i] + "\n";
+}
+
+console.log(output);
+
+fs.writeFile('./sorted_people.txt', output, function(err) {
+  if (err)
+  	console.log("Something went wrong " + err);
+  console.log("Success!");
+});
